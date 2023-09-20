dba024da502b83ab12672d9f8a4ab34c5e13c87f
defwish <58536015+defwish@users.noreply.github.com>
2023-09-20 10:04:32 +0300

    test

--- projectEMv2-main/employee/src/test/java/com/employee/EmployeeApplicationTests.java
+++ projectEMv2-main/employee/src/test/java/com/employee/EmployeeApplicationTests.java
@@ -1,13 +1,55 @@
 package com.employee;
 
+import com.employee.controller.EmployeeController;
+import com.employee.model.Employee;
+import com.employee.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-class EmployeeApplicationTests {
+@ActiveProfiles("test")
+public class EmployeeApplicationTests {
+
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@MockBean
+	private EmployeeRepository employeeRepository;
+
+	@Autowired
+	private EmployeeController employeeController;
+
+
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.openMocks(this);
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat(employeeController).isNotNull();
 	}
 
 }
